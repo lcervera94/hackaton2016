49a8289c31b6b57be139c3064f243d535b801577
lcervera94 <lucaspc94@hotmail.com>
2016-08-19 23:27:51 -0300

    Atualização da tabela dos bancos

--- katze.sql
+++ katze.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Ago-2016 às 02:33
+-- Generation Time: 20-Ago-2016 às 04:26
 -- Versão do servidor: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -28,11 +28,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `lectures` (
   `id` int(11) NOT NULL,
-  `maxUsers` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `tags` varchar(255) NOT NULL,
-  `lecture_time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `schedule` varchar(255) NOT NULL,
+  `lecture_price` decimal(10,0) NOT NULL,
+  `student_price` decimal(10,0) NOT NULL,
+  `max_students` int(11) NOT NULL,
+  `current_students` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,15 +46,18 @@ CREATE TABLE `lectures` (
 
 CREATE TABLE `ratings` (
   `id` int(11) NOT NULL,
-  `lecture_id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `didactism` int(11) NOT NULL,
   `clarity` int(11) NOT NULL,
-  `theme_mastery` int(11) NOT NULL,
+  `didactism` int(11) NOT NULL,
+  `effectiveness` int(11) NOT NULL,
   `lecture_planning` int(11) NOT NULL,
-  `effectiveness` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `theme_mastery` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `lecture_id` int(11) NOT NULL,
+  `average_rating` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -63,9 +70,23 @@ CREATE TABLE `students` (
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `students_per_lecture`
+--
+
+CREATE TABLE `students_per_lecture` (
+  `id` int(11) NOT NULL,
+  `lecture_date` datetime NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `lecture_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -77,10 +98,15 @@ CREATE TABLE `teachers` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `curriculum` varchar(255) NOT NULL,
   `lattes` varchar(255) NOT NULL,
-  `wallet` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `wallet_withdrawal` binary(1) NOT NULL DEFAULT '0',
+  `teacher_payment` decimal(10,0) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -91,7 +117,9 @@ CREATE TABLE `teachers` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -102,28 +130,39 @@ CREATE TABLE `users` (
 -- Indexes for table `lectures`
 --
 ALTER TABLE `lectures`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_LectTeachers` (`teacher_id`);
 
 --
 -- Indexes for table `ratings`
 --
 ALTER TABLE `ratings`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `lecture_id` (`lecture_id`),
-  ADD KEY `teacher_id` (`teacher_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `fk_RatStudents` (`student_id`),
+  ADD KEY `fk_RatTeachers` (`teacher_id`),
+  ADD KEY `fk_RatLectures` (`lecture_id`);
 
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_UserStudents` (`user_id`);
+
+--
+-- Indexes for table `students_per_lecture`
+--
+ALTER TABLE `students_per_lecture`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_StuLec_Students` (`student_id`),
+  ADD KEY `fk_StuLec_Lectures` (`lecture_id`);
 
 --
 -- Indexes for table `teachers`
 --
 ALTER TABLE `teachers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_UserTeachers` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -132,23 +171,41 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `students`
+-- Limitadores para a tabela `lectures`
+--
+ALTER TABLE `lectures`
+  ADD CONSTRAINT `fk_LectTeachers` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
+
+--
+-- Limitadores para a tabela `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `fk_RatLectures` FOREIGN KEY (`lecture_id`) REFERENCES `lectures` (`id`),
+  ADD CONSTRAINT `fk_RatStudents` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  ADD CONSTRAINT `fk_RatTeachers` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
+
+--
+-- Limitadores para a tabela `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `fk_UserStudents` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
 --
--- Constraints for dumped tables
+-- Limitadores para a tabela `students_per_lecture`
 --
+ALTER TABLE `students_per_lecture`
+  ADD CONSTRAINT `fk_StuLec_Lectures` FOREIGN KEY (`lecture_id`) REFERENCES `lectures` (`id`),
+  ADD CONSTRAINT `fk_StuLec_Students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
 
 --
 -- Limitadores para a tabela `teachers`
 --
 ALTER TABLE `teachers`
-  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`id`) REFERENCES `ratings` (`teacher_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_UserTeachers` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
